45d1e3441433bf7ea3d8d8fe039f67de8c52b976
Cyri | Darius <85651634+CyriGolo@users.noreply.github.com>
2024-11-07 23:46:28 +0100

    add layout

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <main class="min-h-screen flex flex-col justify-between">
+    <DefaultHeader />
+    <div class="flex flex-col items-center">
+      <slot />
+    </div>
+    <DefaultFooter />
+  </main>
+</template>
+
+<script setup lang="ts">
+import DefaultHeader from "~/components/Header.vue";
+import DefaultFooter from "~/components/Footer.vue";
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,8 @@
 <template>
-  <h1 class="text-red-200">StudBee</h1>
   <NuxtLink to="/quizz">Start Quizz</NuxtLink>
 </template>
-
-<style>
-button:hover {
-  transform: translateY(.3rem);
-  box-shadow: 0 0 #58cc02;
-}
-
-button {
-  display: block;
-  font-weight: 700;
-  border-radius: 1rem;
-  background-color: #89e219;
-  color: #1b2428;
-  width: 100%;
-  padding: .8rem;
-  text-transform: uppercase;
-  box-shadow: 0 .3rem #58cc02;
-  transition: .2s;
-}
-
-button:hover {
-  transform: translateY(.3rem);
-  box-shadow: 0 0 #58cc02;
-}
+<style lang="scss">
+ * {
+   color: white;
+ }
 </style>
