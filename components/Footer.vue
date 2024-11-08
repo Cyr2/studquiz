096e065f87e583c1c00a519a8ad38f40facbb6ff
Cyr2 <cyrigamingpro@gmail.com>
2024-11-08 14:16:49 +0100

    Update style and color

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer>
+  <footer class="w-full flex flex-col items-center">
     <h2>Made with ❤️</h2>
   </footer>
 </template>
--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="w-full flex flex-col items-center">
     <h1>StudBee</h1>
   </header>
 </template>
