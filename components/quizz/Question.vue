1247c8f71ac654655484aaca2ce269d894ca2a56
Cyr2 <cyrigamingpro@gmail.com>
2024-11-08 15:45:23 +0100

    Replace CSS by Tailwind class

--- components/quizz/Question.vue
+++ components/quizz/Question.vue
@@ -1,20 +1,18 @@
 <template>
   <main class="flex flex-col items-center text-center gap-8">
-    <h3>{{ question.question }}</h3>
+    <h3 class="text-2xl font-medium">{{ question.question }}</h3>
     <ul class="flex flex-col items-center w-full gap-4">
-      <li v-for="answer in shuffledAnswers" :key="answer.id" :class="{'correct': questionDone && answer.correct, 'shadow-defaultInputHover': questionDone}" class="relative w-full uppercase font-bold p-3.5 rounded-2xl border-2 border-blue-dark-light bg-blue-dark-dark shadow-defaultInput hover:translate-y-1 hover:shadow-defaultInputHover">
-        <input type="radio" :id="answer.id" :value="answer.id" :name="question.id" v-model="selectedAnswers" required :disabled="questionDone" class="opacity-0 w-full h-full absolute top-0 left-0 cursor-pointer"  />
-        <label :for="answer.id">{{ answer.answer }}</label>
+      <li v-for="answer in shuffledAnswers" :key="answer.id" class="relative w-full uppercase font-bold p-3.5 rounded-2xl border-2 border-blue-dark-light bg-blue-dark-dark" :class="[{'!border-green-500 !bg-blue-dark-base': correctAnswer !== null && answer.correct}, correctAnswer === null ? 'shadow-defaultInput hover:shadow-defaultInputHover translate-y-0 hover:translate-y-1' : 'translate-y-1']">
+        <input type="radio" :id="answer.id" :value="answer.id" :name="question.id" v-model="selectedAnswers" required :disabled="correctAnswer !== null" class="opacity-0 w-full h-full absolute top-0 left-0 cursor-pointer"  />
+        <label :for="answer.id" :class="{'!text-green-500': correctAnswer !== null && answer.correct}">{{ answer.answer }}</label>
       </li>
     </ul>
-    <ButtonDefault @click="displayResult" v-if="!questionDone">Suivant</ButtonDefault>
-    <ButtonDefault @click="nextQuestion" v-else>Prochaine question</ButtonDefault>
+    <ButtonDefault @click="displayResult" v-if="correctAnswer === null">Valider la réponse</ButtonDefault>
+    <ButtonDefault @click="nextQuestion" v-else>Continuer</ButtonDefault>
   </main>
 </template>
 
 <script setup lang="ts">
-import { onMounted } from 'vue';
-
 const props = defineProps<{
   question: {
     id: number;
@@ -30,36 +28,33 @@ const shuffledAnswers = ref(props.question.answers);
 
 const selectedAnswers = ref<string | null>(null);
 
-const questionDone = ref(false);
+const correctAnswer = ref<boolean | null>(null);
 
 onMounted(() => {
   shuffledAnswers.value = [...props.question.answers].sort(() => Math.random() - 0.5);
 });
 
-const displayResult = () => {
-  questionDone.value = true;
+const displayResult = () =>   {
+  correctAnswer.value = selectedAnswers.value === props.question.answers.find((answer) => answer.correct)?.id;
+  console.log(correctAnswer.value);
 };
 
 const nextQuestion = () => {
-  questionDone.value = false;
+  props.updateQuestion(correctAnswer.value);
+  correctAnswer.value = null;
   selectedAnswers.value = null;
-  props.updateQuestion();
   shuffledAnswers.value = [...props.question.answers].sort(() => Math.random() - 0.5);
 };
 </script>
 
 <style>
 li:has(input:checked) {
-  color: #1e97d4;
   box-shadow: 0 0;
   transform: translateY(.25rem);
   border-color: #1e97d4;
   background-color: #253339;
-}
-
-.correct {
-  color: #58cc02 !important;
-  border: 2px solid #58cc02 !important;
-  background-color: #253339;
+  label {
+    color: #1e97d4;
+  }
 }
 </style>
