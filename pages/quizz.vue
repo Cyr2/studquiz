5cee5e37b14dac04acf9adb97090f3dcd4572a2e
Cyr2 <cyrigamingpro@gmail.com>
2024-11-08 15:43:52 +0100

    Add streak comment on progressbar

--- pages/quizz.vue
+++ pages/quizz.vue
@@ -1,7 +1,15 @@
 <template>
   <main class="w-[50vw] flex flex-col items-center gap-8">
-    <div v-if="questionRemaining > 0">
-      <UProgress :value="currentQuestion + 1" :max="quizzData.length" />
+    <div v-if="questionRemaining > 0" class="w-full flex flex-col gap-8">
+      <UProgress :value="currentQuestion + 1" :max="quizzData.length" :color="progressColor">
+        <template #indicator="{ percent }">
+          <div class="text-right" :style="{ width: `${percent}%` }">
+            <p v-if="streak >= 4 && streak >= Math.floor((80 / 100) * quizzData.length)" class="text-red-500 text-sm">🔥 Trop fort! <span class="text-xs text-red-500">(x{{ streak }})</span></p>
+            <p v-else-if="streak >= 3 && streak >= Math.floor((40 / 100) * quizzData.length)" class="text-blue-500 text-sm">Génial <span class="text-xs text-blue-500">(x{{ streak }})</span></p>
+            <p v-else-if="streak >= 2 && streak >= Math.floor((20 / 100) * quizzData.length)" class="text-sm">Pas mal <span class="text-xs text-green-500">(x{{ streak }})</span></p>
+          </div>
+        </template>
+      </UProgress>
       <QuizzQuestion :question="currentQuestionData" :updateQuestion="updateQuestion" />
     </div>
     <div v-else>
@@ -13,9 +21,10 @@
 <script setup lang="ts">
 import data from "~/data/quizz.json";
 
-const quizzData = data.quizz;
+const quizzData = data.quizz || [];
 
 const currentQuestion = ref(0);
+const streak = ref(0);
 
 const currentQuestionData = computed(() => {
   return quizzData[currentQuestion.value];
@@ -25,7 +34,21 @@ const questionRemaining = computed(() => {
   return quizzData.length - currentQuestion.value;
 });
 
-const updateQuestion = () => {
+const updateQuestion = (correctAnswer) => {
+  if (correctAnswer) {
+    streak.value++;
+  } else {
+    streak.value = 0;
+  }
   currentQuestion.value++;
 };
+
+const progressColor = computed(() => {
+  switch (true) {
+    case streak.value >= 4 &&streak.value >= Math.floor((80 / 100) * quizzData.length): return 'red';
+    case streak.value >= 3 && streak.value >= Math.floor((40 / 100) * quizzData.length): return 'blue';
+    default: return 'primary'
+  }
+})
+
 </script>
