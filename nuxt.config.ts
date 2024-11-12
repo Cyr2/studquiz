57ab9e5a31d2837aef291b4c615fae877b924c3b
Cyr2 <cyrigamingpro@gmail.com>
2024-11-12 16:37:14 +0100

    Config env api key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,23 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui'],
+  modules: ['@nuxt/ui', '@pinia/nuxt'],
   css: [
     {
       src: '~/assets/scss/global.scss',
       lang: 'scss'
     }
   ],
+  runtimeConfig: {
+    public : {
+      togetherApi: process.env.NUXT_TOGETHER_API || ''
+    }
+  },
+  router: {
+    middleware: {
+      'manifest-route-rule': {
+        override: true
+      }
+    }
+  }
 })
