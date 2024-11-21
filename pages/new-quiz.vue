ba29813a371bd14734b749eba3566ec0a39651c6
Cyr2 <cyrigamingpro@gmail.com>
2024-11-21 17:27:27 +0100

    Add difficulty

--- pages/new-quiz.vue
+++ pages/new-quiz.vue
@@ -1,6 +1,6 @@
 <template>
   <p v-if="loading">Loading...</p>
-  <form @submit.prevent="createQuiz" class="flex flex-col gap-4" v-else>
+  <form @submit.prevent="createQuiz" class="flex flex-col gap-4" v-else v-auto-animate>
     <div class="flex flex-col gap-1">
       <label for="subject">Sujet :</label>
       <input type="text" v-model="subject" placeholder="Entrez un sujet" id="subject" class="p-3.5 rounded-2xl font-bold bg-blue-dark-dark border-2 border-blue-dark-light outline-none focus:border-green-500" required>
@@ -8,8 +8,14 @@
     <div class="flex flex-col gap-1">
       <label for="limit">Nombre de questions :</label>
       <span class="flex items-center gap-2">
-        <URange :min="4" :max="10" :step="2" v-model="questionLimit" id="limit"/>
+        <URange :min="4" :max="10" v-model="questionLimit" id="limit"/>
         <span>{{ questionLimit }}</span>
+      </span>
+    </div>
+    <div>
+      <label for="difficulty">Difficulté :</label>
+      <span>
+        <UTabs :items="difficulty" v-model="selectedDifficulty" id="difficulty" />
       </span>
     </div>
     <ButtonDefault type="submit">Créer un nouveau quiz</ButtonDefault>
@@ -18,16 +24,39 @@
 
 <script lang="ts" setup>
 import { setData, data, loading } from "~/hooks/useQuizPrompt"
+import { useRoute, useRouter } from 'vue-router';
+
 loading.value = false;
 
+const difficulty = [{
+  label: 'Facile',
+  icon: 'clarity:star-line',
+}, {
+  label: 'Moyen',
+  icon: 'clarity:half-star-solid',
+}, {
+  label: 'Difficile',
+  icon: 'clarity:star-solid',
+}]
+
 const router = useRouter();
+const route = useRoute();
 
 const quizzStore = useQuizzStore();
 const subject = ref('');
 const questionLimit = ref(4);
+const selectedDifficulty = computed({
+  get() {
+    const index = difficulty.findIndex(item => item.label === route.query.difficulty);
+    return index === -1 ? 0 : index;
+  },
+  set(value) {
+    router.replace({ query: { difficulty: difficulty[value].label } });
+  }
+});
 
 const createQuiz = async () => {
-  await setData(subject.value, questionLimit.value);
+  await setData(subject.value, questionLimit.value, difficulty[selectedDifficulty.value].label);
   quizzStore.setQuizzData(data.value);
   router.push({ path: '/quizz' });
 };
