20d8040adf9a92e881f8691b23a3e7d528eb2814
Cyr2 <cyrigamingpro@gmail.com>
2024-11-22 16:24:28 +0100

    Add animation (auto animate)

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
   <NuxtLayout>
     <NuxtPage />
+    <UNotifications />
   </NuxtLayout>
 </template>
+
+<script setup>
+import { useAutoAnimate } from "@formkit/auto-animate/vue";
+</script>
