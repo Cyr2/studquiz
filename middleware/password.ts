f59fa8ce15f7143e8deb4a03ca25ce4f135c3431
Cyr2 <dpessin@edenschool.fr>
2024-11-24 23:25:09 +0100

    update dependencies and remove console log

--- middleware/password.ts
+++ middleware/password.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
   const loginStore = useLogin();
-  console.log("Middleware check:", loginStore.logged);
 
   if (!loginStore.logged) {
     return navigateTo('/password');
