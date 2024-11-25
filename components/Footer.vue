6989369b19a983b7af47d14ce6a5bd64a3c96477
Cyr2 <cyrigamingpro@gmail.com>
2024-11-25 14:41:55 +0100

    Update footer & header

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,5 +1,8 @@
 <template>
   <footer class="w-full flex flex-col items-center p-6">
-    <h2>Made with ❤️</h2>
+    <h2><a href="https://dpessin.tech" class="text-sm">Made with ❤️ <span class="text-xs">by Cyri</span></a></h2>
+
   </footer>
 </template>
+<script setup lang="ts">
+</script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
   <header class="w-full flex flex-col items-center p-6">
-    <h1><NuxtLink to="/">StudBee</NuxtLink></h1>
+    <h1><NuxtLink to="/">StudQuiz</NuxtLink></h1>
   </header>
 </template>
