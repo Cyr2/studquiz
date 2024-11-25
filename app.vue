9cc65abbb50ef9183f486a177d62998a14e78ccc
Cyr2 <cyrigamingpro@gmail.com>
2024-11-25 14:26:43 +0100

    Add vercel Speed Insights

--- app.vue
+++ app.vue
@@ -4,9 +4,11 @@
     <UNotifications />
   </NuxtLayout>
   <Analytics/>
+  <SpeedInsights/>
 </template>
 
 <script setup>
 import { useAutoAnimate } from "@formkit/auto-animate/vue";
 import { Analytics } from '@vercel/analytics/nuxt'
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
