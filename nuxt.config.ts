2d7e271b23d461e26eb118e8d53c42b2e67fd50e
Cyr2 <cyrigamingpro@gmail.com>
2025-02-19 11:21:59 +0100

    update for pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,8 +21,53 @@ export default defineNuxtConfig({
         { property: 'og:description', content: 'StudQuiz permet de créer des quiz sur mesure 100% générés par une intelligence artificielle.' },
         { property: 'og:type', content: 'website' },
         { property: 'og:url', content: 'https://studquiz.vercel.app' }
+      ],
+      link: [
+        { rel: 'manifest', href: '/manifest.webmanifest' }
       ]
     }
+  },
+  pwa: {
+    manifest: {
+      name: 'StudQuiz',
+      short_name: 'StudQuiz',
+      description: 'StudQuiz permet de créer des quiz sur mesure 100% générés par une intelligence artificielle.',
+      icons: [
+        {
+          src: 'mely-dark.webp',
+          sizes: '192x192',
+          type: 'image/webp',
+        },
+        {
+          src: 'mely.webp',
+          sizes: '512x512',
+          type: 'image/webp',
+        },
+      ],
+      start_url: '/',
+      display: 'standalone',
+      background_color: '#ffffff',
+      theme_color: '#000000',
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ url }) => url.origin === 'https://studquiz.vercel.app',
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'api-cache',
+            expiration: {
+              maxEntries: 50,
+              maxAgeSeconds: 30 * 24 * 60 * 60, // 30 jours
+            },
+          },
+        },
+      ],
+    },
+    client: {
+      installPrompt: true,
+      periodicSyncForUpdates: 3600,
+    },
   },
   runtimeConfig: {
     geminiApi: process.env.NUXT_GEMINI_API || '',
